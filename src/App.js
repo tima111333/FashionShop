978f9bbeca2c50db86f9cad8f14c8dd716759c4b
tima111333 <super.tima2001@mail.ru>
2024-03-18 13:28:29 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Header from './components/header/Header'
+import Promo from './components/promo/Promo';
+
+
+function App() {
+
+
+
+
+
+  return (
+    <div className="App">
+      <Header/>
+      <Promo />
+    </div>
+  )
+
+
+}
+
+export default App;
---
+++ src/components/promo/Promo.jsx
@@ -0,0 +1,14 @@
+import './Promo.css'
+
+const Promo = () => {
+    return (<section className="Promo">
+        <div className="container">
+            <div className="promo__content">
+                <div className="promo__text">TEXT</div>
+                <div className="promo__img">IMG</div>
+            </div>
+        </div>
+    </section> );
+}
+ 
+export default Promo ;
