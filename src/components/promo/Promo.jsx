1daf2a00cbbb8070c0c5e647fecfe50a5ecdccb1
tima111333 <super.tima2001@mail.ru>
2024-03-19 19:40:53 +0300

    добавление кнопки , реализация promo__content

--- src/components/promo/Promo.jsx
+++ src/components/promo/Promo.jsx
@@ -1,14 +1,32 @@
 import './Promo.css'
+import promoImg from "./../../img/images/header-img.jpg"
 
 const Promo = () => {
     return (<section className="Promo">
         <div className="container">
             <div className="promo__content">
-                <div className="promo__text">TEXT</div>
-                <div className="promo__img">IMG</div>
+                <div className="promo__text">
+                    <div className="promo__title">
+                        <span className='highlight'>LET’S</span>
+                        EXPLORE UNIQUE CLOTHES.
+                        </div>
+                    <div className="promo__desc">
+                        Live for Influential and Innovative fashion! 
+                        </div>
+                    <div className="promo__btn-wrapper">
+                        <a href="#!" className="promo__btn">
+                            Shop Now
+                            </a>
+                    </div>
+
+
+                </div>
+                <div className="promo__img">
+                    <img src={promoImg} alt="" />
+                </div>
             </div>
         </div>
-    </section> );
+    </section>);
 }
- 
-export default Promo ;
+
+export default Promo;  
